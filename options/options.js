1cb2961a64688c0a0c809653353b66eb8a009ec9
Crowdbotics <team@crowdbotics.com>
2021-08-14 07:20:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_wind_29801Identifier",
+  url: "https://proud_wind_29801Identifier.botics.co",
+  api: "https://proud_wind_29801Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
